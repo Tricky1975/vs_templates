e2b42f540fc1da1bae2c05836a248531e00ad4a3
Tricky1975 <facebook@tbbs.nl>
2019-07-23 00:14:25 +0200

    GTK#

---
+++ Template_GTK_Sharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace Template_GTK_Sharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
